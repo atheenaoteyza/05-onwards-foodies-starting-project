cb5b3d12bfbceed1d1b353a387d55a345e09f9c1
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2025-01-10 19:26:09 +0400

    feat: css modules for header & main component

--- public/component/header.js
+++ public/component/header.js
@@ -1,28 +1,28 @@
 import Link from "next/link";
 import Logo from "@/public/images/logo.png";
+import classes from "./header.module.css";
+import MainHeaderBackground from "./main-header-background";
 
 export default function Header() {
   return (
-    <header>
-      <Link href="/">
-        <img src={Logo.src}></img>
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Homepage</Link>{" "}
-          </li>
-          <li>
-            <Link href="/meals/">Browse a Meal</Link>
-          </li>
-          <li>
-            <Link href="/meals/share">Share a Meal</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground></MainHeaderBackground>
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <img src={Logo.src}></img>NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals/">Browse a Meal</Link>
+            </li>
+
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
