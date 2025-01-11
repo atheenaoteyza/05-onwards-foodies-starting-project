72969264a133bc9ec2bbf0338ac8e35f7e695f09
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2025-01-11 22:14:22 +0400

    chore: imported images from assets

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import burger from "@/assets/burger.jpg";
+import curry from "@/assets/curry.jpg";
+import dumplings from "@/assets/dumplings.jpg";
+import macncheese from "@/assets/macncheese.jpg";
+import pizza from "@/assets/pizza.jpg";
+import schnitzel from "@/assets/schnitzel.jpg";
+import tomato from "@/assets/tomato-salad.jpg";
+
 export default function Home() {
   return (
     <>
